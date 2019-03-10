a5739bd1ee936aa85813e27bcb8dee311c07a61c
Calvin Hall <123clownvin@gmail.com>
2019-03-09 23:16:10 -0600

    Added formatAsPercent overload

--- ClownvinsUtility/src/com/clownvin/util/Util.java
+++ ClownvinsUtility/src/com/clownvin/util/Util.java
@@ -2,6 +2,10 @@ package com.clownvin.util;
 
 
 public class Util {
+   public static String formatAsPercent(double value) {
+      return formatAsPercent(value, 3);
+   }
+   
    public static String formatAsPercent(double value, int decimalPlaces) {
       return String.format("%."+decimalPlaces+"f", value * 100);
    }
