0bf649f18ed342747f163d50acc0b22bb014f100
Calvin Hall <123clownvin@gmail.com>
2019-03-09 22:58:42 -0600

    Cleanup

--- ClownvinsUtility/src/com/git/clownvin/time/StopWatch.java
+++ ClownvinsUtility/src/com/git/clownvin/time/StopWatch.java
@@ -23,19 +23,19 @@ import java.util.concurrent.TimeUnit;
  *
  */
 public class StopWatch {
-
+   
    /**
     * Globally instanced {@code StopWatch}, meant to be statically imported in
     * certain use-cases.
-    * 
+    *
     * @since 1.0
     */
    public static final StopWatch GLOBAL_STOPWATCH = new StopWatch();
-   
+
    /**
     * The value of {@code System.nanoTime()} from when {@link #start} was last
     * called, plus any pauses.
-    * 
+    *
     * @see #start()
     * @see #start(long)
     * @see #restart()
@@ -48,7 +48,7 @@ public class StopWatch {
    /**
     * The value of {@code System.nanoTime()} from when {@link #pause} was last
     * called.
-    * 
+    *
     * @see #pause()
     * @see #unpause()
     * @since 1.0
@@ -58,17 +58,17 @@ public class StopWatch {
     * How much time should pass, not including pauses, before the {@link #alarm}
     * notifies all waiting on this {@code StopWatch} and the {@code StopWatch}
     * should be considered expired.
-    * 
+    *
     * @see #alarm
     * @see #expired()
     * @see #timeLeft()
     * @since 1.0
     */
    protected volatile long timerLength = 0;
-
+   
    /**
     * Whether or not this StopWatch is stopped.
-    * 
+    *
     * @see #start()
     * @see #start(long)
     * @see #restart()
@@ -80,27 +80,27 @@ public class StopWatch {
    protected volatile boolean stopped = true;
    /**
     * Whether or not this StopWatch is paused.
-    * 
+    *
     * @see #isPaused()
     * @see #pause()
     * @see #unpause()
     * @since 1.0
     */
    protected volatile boolean paused  = false;
-   
+
    /**
     * Exists to call {@link #notifyAll()} on this {@code StopWatch} whenever this
     * {@code StopWatch} becomes expired.
-    * 
+    *
     * @see #expired()
     * @see #notifyAll()
     * @since 1.0
     */
    protected Thread alarm;
-   
+
    /**
     * The actual logic block for {@link #alarm}.
-    * 
+    *
     * @see #alarm
     * @since 1.0
     */
@@ -127,11 +127,11 @@ public class StopWatch {
          }
       }
    };
-   
+
    /**
     * Creates a new {@code StopWatch} instance, creating a new {@link #alarm} at
     * the same time.
-    * 
+    *
     * @see #alarm
     * @since 1.0
     */
@@ -140,11 +140,11 @@ public class StopWatch {
       alarm.setName("StopWatch@" + hashCode() + ".alarm");
       alarm.start();
    }
-
+   
    /**
     * Checks if this {@code StopWatch} is paused. If so, it throws an
     * {@link IllegalStateException}.
-    * 
+    *
     * @see #isPaused()
     * @since 1.0
     */
@@ -153,11 +153,11 @@ public class StopWatch {
          throw new IllegalStateException("StopWatch is paused.");
       }
    }
-
+   
    /**
     * Checks if this {@code StopWatch} is started. If so, throws an
     * {@link IllegalStateException}.
-    * 
+    *
     * @see #isStopped()
     * @since 1.0
     */
@@ -166,11 +166,11 @@ public class StopWatch {
          throw new IllegalStateException("StopWatch is already started.");
       }
    }
-
+   
    /**
     * Checks if this {@code StopWatch} is stopped. If so, throws an
     * {@link IllegalStateException}.
-    * 
+    *
     * @see #isStopped()
     * @since 1.0
     */
@@ -179,11 +179,11 @@ public class StopWatch {
          throw new IllegalStateException("StopWatch is not started.");
       }
    }
-
+   
    /**
     * Checks if this {@code StopWatch} is unpaused. If so, throws an
     * {@link IllegalStateException}.
-    * 
+    *
     * @see #isPaused()
     * @since 1.0
     */
@@ -192,11 +192,11 @@ public class StopWatch {
          throw new RuntimeException("StopWatch is not paused!");
       }
    }
-
+   
    /**
     * Returns {@code true} this {@code StopWatch} is expired by checking
     * {@link #timeLeft()}.
-    * 
+    *
     * @return true if {@code timeLeft() < 0}, false otherwise
     * @see #timeLeft()
     * @since 1.0
@@ -204,10 +204,10 @@ public class StopWatch {
    public boolean expired() {
       return timeLeft() < 0;
    }
-
+   
    /**
     * Getter for {@link #paused}.
-    * 
+    *
     * @return paused
     * @see #paused
     * @since 1.0
@@ -215,10 +215,10 @@ public class StopWatch {
    public boolean isPaused() {
       return paused;
    }
-
+   
    /**
     * Getter for {@link #stopped}.
-    * 
+    *
     * @return stopped
     * @see #stopped
     * @since 1.0
@@ -226,11 +226,11 @@ public class StopWatch {
    public boolean isStopped() {
       return stopped;
    }
-
+   
    /**
     * Pauses this {@code StopWatch}. Useful for executing code outside the scope of
     * what is intended to be monitored.
-    * 
+    *
     * @see #unpause()
     * @since 1.0
     */
@@ -239,12 +239,12 @@ public class StopWatch {
       checkPaused();
       paused = true;
    }
-
+   
    /**
     * Resets this {@code StopWatch} to an effectively default state. Will need to
     * be restarted with {@link #start start}, {@link #restart restart} or
     * {@link #restartPaused restartPaused}.
-    * 
+    *
     * @return this {@code StopWatch}, for method chaining
     * @since 1.0
     */
@@ -253,21 +253,21 @@ public class StopWatch {
       paused = false;
       return this;
    }
-
+   
    /**
     * Restarts this {@code StopWatch} with a timer length of 0.
-    * 
+    *
     * @return this {@code StopWatch}, for method chaining
     * @since 1.0
     */
    public StopWatch restart() {
       return restart(0);
    }
-
+   
    /**
     * Restarts this {@code StopWatch} with the specified timer length, ignoring
     * whether or not this {@code StopWatch} was started already.
-    * 
+    *
     * @param timerLength the new timer length to start with
     * @return this {@code StopWatch}, for method chaining
     * @since 1.0
@@ -276,7 +276,7 @@ public class StopWatch {
       reset().start(timerLength);
       return this;
    }
-
+   
    /**
     * Restarts this {@code StopWatch} in a paused state with a timer length of 0.
     * <p>
@@ -284,22 +284,23 @@ public class StopWatch {
     * since it will allow the code executing to chose when to start and stop this
     * {@code StopWatch} without having to immediately pause it itself to achieve
     * the same behavior.
-    * 
+    *
     * @return this {@code StopWatch}, for method chaining
     * @since 1.0
     */
    public StopWatch restartPaused() {
       return restartPaused(0);
    }
-   
+
    /**
-    * Restarts this {@code StopWatch} in a paused state with the specified timer length.
+    * Restarts this {@code StopWatch} in a paused state with the specified timer
+    * length.
     * <p>
     * Starting paused is useful for calling {@link #runForDuration(Runnable)},
     * since it will allow the code executing to chose when to start and stop this
     * {@code StopWatch} without having to immediately pause it itself to achieve
     * the same behavior.
-    * 
+    *
     * @param timerLength the new timer length to start with
     * @return this {@code StopWatch}, for method chaining
     * @since 1.0
@@ -309,18 +310,22 @@ public class StopWatch {
       start(timerLength);
       return this;
    }
-   
+
    /**
-    * Will continuously execute the {@link Runnable} specified until this {@code StopWatch}es
-    * timer is expired.
-    * <p>This is useful for executing code in succession, but only for a while.
-    * When used in conjunction with pausing, you can also guarantee specific that code executes for
-    * approximately the length of the timer
-    * <p>If you need very precise control over what segments of code affect this {@code StopWatch}, you can
-    * call this method while in a paused state. Doing this means that you will need to manually unpause this
-    * {@code StopWatch} inside the specified {@code Runnable}.
-    * 
-    * @param code the code to be executed for the remaining duration of this {@code StopWatch}.
+    * Will continuously execute the {@link Runnable} specified until this
+    * {@code StopWatch}es timer is expired.
+    * <p>
+    * This is useful for executing code in succession, but only for a while. When
+    * used in conjunction with pausing, you can also guarantee specific that code
+    * executes for approximately the length of the timer
+    * <p>
+    * If you need very precise control over what segments of code affect this
+    * {@code StopWatch}, you can call this method while in a paused state. Doing
+    * this means that you will need to manually unpause this {@code StopWatch}
+    * inside the specified {@code Runnable}.
+    *
+    * @param code the code to be executed for the remaining duration of this
+    *             {@code StopWatch}.
     * @return this {@code StopWatch}, for method chaining
     * @since 1.0
     */
@@ -330,9 +335,11 @@ public class StopWatch {
       }
       return this;
    }
-   
+
    /**
-    * Sets the timer length of this {@code StopWatch} and notifies the alarm to start ticking.
+    * Sets the timer length of this {@code StopWatch} and notifies the alarm to
+    * start ticking.
+    * 
     * @param timerLength new timer length
     * @return this {@code StopWatch}, for method chaining
     * @since 1.0
@@ -347,21 +354,24 @@ public class StopWatch {
       }
       return this;
    }
-   
+
    /**
-    * Starts this {@code StopWatch}. Effectively calls {@link #start(long) start(0)}.
+    * Starts this {@code StopWatch}. Effectively calls {@link #start(long)
+    * start(0)}.
+    * 
     * @return this {@code StopWatch}, for method chaining
     * @since 1.0
     */
    public StopWatch start() {
       return start(0);
    }
-
+   
    /**
     * Starts this {@code StopWatch} with the specified timer length.
-    * <p>Will throw an {@code IllegalStateException} if this {@code StopWatch}
-    * is already started.
-    * 
+    * <p>
+    * Will throw an {@code IllegalStateException} if this {@code StopWatch} is
+    * already started.
+    *
     * @param timerLength new timer length
     * @return this {@code StopWatch}, for method chaining
     * @since 1.0
@@ -373,12 +383,13 @@ public class StopWatch {
       startTime = System.nanoTime();
       return this;
    }
-
+   
    /**
     * Stops this {@code StopWatch}, and returns the time elapsed.
-    * <p>Will unpause if paused, and will throw an {@code IllegalStateException} if
+    * <p>
+    * Will unpause if paused, and will throw an {@code IllegalStateException} if
     * this {@code StopWatch} is not started.
-    * 
+    *
     * @return the time elapsed from start
     * @see #timeElapsed()
     * @see #timeElapsed(TimeUnit)
@@ -392,20 +403,21 @@ public class StopWatch {
       stopped = false;
       return timeElapsed();
    }
-
+   
    /**
-    * Simply calls {@link #timeElapsed(TimeUnit) timeElapsed(TimeUnit.MILLISECONDS)}.
-    * 
+    * Simply calls {@link #timeElapsed(TimeUnit)
+    * timeElapsed(TimeUnit.MILLISECONDS)}.
+    *
     * @return time since the start, in milliseconds
     */
    public long timeElapsed() {
       return timeElapsed(MILLISECONDS);
    }
-   
+
    /**
-    * Returns the time since the start, after converting it from nanoseconds to 
-    * the specified {@link TimeUnit}.
-    * 
+    * Returns the time since the start, after converting it from nanoseconds to the
+    * specified {@link TimeUnit}.
+    *
     * @param unit {@code TimeUnit} to convert the time to.
     * @return time since the start, in the specified {@code TimeUnit}
     */
@@ -413,15 +425,15 @@ public class StopWatch {
       checkStopped();
       return unit.convert(System.nanoTime() - (paused ? startTime + (System.nanoTime() - pauseTime) : startTime), NANOSECONDS);
    }
-
+   
    /**
-    * 
+    *
     * @return
     */
    public synchronized long timeLeft() {
       return timerLength - timeElapsed();
    }
-
+   
    /**
     *
     */
